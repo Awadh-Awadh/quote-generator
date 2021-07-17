b8657e457fb64b681b5f31bd94c1262c3db817fc
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-07-17 17:22:34 +0300

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LogoComponent } from './logo/logo.component';
+import { HeroComponent } from './hero/hero.component';
+import { FormComponent } from './form/form.component';
+import { ItemComponent } from './item/item.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoComponent,
+    HeroComponent,
+    FormComponent,
+    ItemComponent,
+    QuoteDetailComponent
   ],
   imports: [
     BrowserModule
